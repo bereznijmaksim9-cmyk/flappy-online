640ad3359bd6bd4acac170444abef4a22feab84e
Skvizyx <bereznijmaksim9@gmail.com>
2026-07-23 13:54:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,52 +7,65 @@ app.use(express.static('public'));
 
 const rooms = {};
 
+// Функция рассылки списка комнат всем в главном меню
+function broadcastRooms() {
+    const publicRooms = [];
+    for (let id in rooms) {
+        publicRooms.push({ id: id, count: Object.keys(rooms[id].players).length });
+    }
+    io.emit('roomList', publicRooms);
+}
+
 io.on('connection', (socket) => {
-    // Подключение к комнате
-    socket.on('joinRoom', ({ roomId, playerName }) => {
+    // При подключении сразу шлем список лобби
+    socket.emit('roomList', Object.keys(rooms).map(id => ({ id, count: Object.keys(rooms[id].players).length })));
+
+    socket.on('joinRoom', ({ roomId, playerName, color, skin }) => {
         socket.join(roomId);
         if (!rooms[roomId]) rooms[roomId] = { players: {} };
         
-        rooms[roomId].players[socket.id] = { name: playerName, score: 0, alive: true, y: 250 };
+        // Теперь отслеживаем и пройденную дистанцию
+        rooms[roomId].players[socket.id] = { name: playerName, score: 0, alive: true, y: 300, distance: 0, color, skin };
+        
+        broadcastRooms();
         io.to(roomId).emit('roomState', rooms[roomId].players);
     });
 
-    // Постоянное обновление состояния (координаты, жизнь, счет)
     socket.on('updateState', (data) => {
-        const { roomId, score, alive, y } = data;
+        const { roomId, score, alive, y, distance } = data;
         if (rooms[roomId] && rooms[roomId].players[socket.id]) {
-            rooms[roomId].players[socket.id].score = score;
-            rooms[roomId].players[socket.id].alive = alive;
-            rooms[roomId].players[socket.id].y = y;
+            const p = rooms[roomId].players[socket.id];
+            p.score = score; p.alive = alive; p.y = y; p.distance = distance;
             
-            // Рассылаем всем остальным в комнате, чтобы они видели твой полет
+            // Рассылаем остальным
             socket.to(roomId).emit('playerMoved', { 
-                id: socket.id, 
-                score: score, 
-                alive: alive, 
-                y: y 
+                id: socket.id, score, alive, y, distance 
             });
         }
     });
 
-    // Кнопка выхода из комнаты
     socket.on('leaveRoom', ({ roomId }) => {
         socket.leave(roomId);
-        if (rooms[roomId] && rooms[roomId].players[socket.id]) {
-            delete rooms[roomId].players[socket.id];
-            io.to(roomId).emit('roomState', rooms[roomId].players);
+        removePlayer(roomId, socket.id);
+    });
+
+    socket.on('disconnect', () => {
+        for (const roomId in rooms) {
+            removePlayer(roomId, socket.id);
         }
     });
 
-    // Отключение от сервера (закрыл вкладку/приложение)
-    socket.on('disconnect', () => {
-        for (const roomId in rooms) {
-            if (rooms[roomId]?.players[socket.id]) {
-                delete rooms[roomId].players[socket.id];
+    function removePlayer(roomId, socketId) {
+        if (rooms[roomId] && rooms[roomId].players[socketId]) {
+            delete rooms[roomId].players[socketId];
+            if (Object.keys(rooms[roomId].players).length === 0) {
+                delete rooms[roomId]; // Удаляем пустую комнату
+            } else {
                 io.to(roomId).emit('roomState', rooms[roomId].players);
             }
+            broadcastRooms();
         }
-    });
+    }
 });
 
 const PORT = process.env.PORT || 3000;
