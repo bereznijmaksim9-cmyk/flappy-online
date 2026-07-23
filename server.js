c646c184e3a069ee6a620c009be76989d912b42c
bereznijmaksim9-cmyk <bereznijmaksim9@gmail.com>
2026-07-23 13:15:11 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+ Указываем папку, где будет лежать игра (index.html)
+app.use(express.static('public'));
+
+const rooms = {};
+
+io.on('connection', (socket) = {
+    socket.on('joinRoom', ({ roomId, playerName }) = {
+        socket.join(roomId);
+        if (!rooms[roomId]) rooms[roomId] = { players {} };
+        
+        rooms[roomId].players[socket.id] = { name playerName, score 0, alive true };
+        io.to(roomId).emit('roomState', rooms[roomId].players);
+    });
+
+    socket.on('scoreUpdate', ({ roomId, score }) = {
+        if (rooms[roomId] && rooms[roomId].players[socket.id]) {
+            rooms[roomId].players[socket.id].score = score;
+            io.to(roomId).emit('roomState', rooms[roomId].players);
+        }
+    });
+
+    socket.on('playerDied', ({ roomId }) = {
+        if (rooms[roomId] && rooms[roomId].players[socket.id]) {
+            rooms[roomId].players[socket.id].alive = false;
+            io.to(roomId).emit('roomState', rooms[roomId].players);
+        }
+    });
+
+    socket.on('disconnect', () = {
+        for (const roomId in rooms) {
+            if (rooms[roomId].players[socket.id]) {
+                delete rooms[roomId].players[socket.id];
+                io.to(roomId).emit('roomState', rooms[roomId].players);
+            }
+        }
+    });
+});
+
+ Используем порт среды (для хостинга) или 3000 локально
+const PORT = process.env.PORT  3000;
+http.listen(PORT, () = console.log(`Сервер запущен на порту ${PORT}`));
